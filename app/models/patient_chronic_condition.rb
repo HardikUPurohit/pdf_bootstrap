53cfe1d3fbe6a6c1ed8833e99249b530db28bb96
hardik <hardik.purohit@botreetechnologies.com>
2019-04-24 17:38:22 +0530

    added seed data

--- app/models/patient_chronic_condition.rb
+++ app/models/patient_chronic_condition.rb
@@ -1,4 +1,4 @@
 class PatientChronicCondition < ApplicationRecord
-  belongs_to :chronic_condition, foreign_key: :diagnosis_id, primary_key: :diagnosis_id
+  belongs_to :chronic_condition, class_name: 'Diagnosis'
   belongs_to :patient
 end
